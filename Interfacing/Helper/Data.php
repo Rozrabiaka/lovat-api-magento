618526e7e498576aff16d8784bda982bb432f54f
Rozrabiaka <denmaksiura@gmail.com>
2021-07-01 11:19:01 +0300

    updates for php 5.5

--- Interfacing/Helper/Data.php
+++ Interfacing/Helper/Data.php
@@ -2,8 +2,6 @@
 
 class Lovat_Interfacing_Helper_Data extends Mage_Core_Helper_Abstract
 {
-	const ATTRIBUTES = array('from', 'to');
-
 	public function validationApiGetDataFromTo($from, $to)
 	{
 		if ($this->is_Date($from) != false and $this->is_Date($to) != false) {
@@ -19,7 +17,8 @@ class Lovat_Interfacing_Helper_Data extends Mage_Core_Helper_Abstract
 
 	public function importantAttributes($attributes)
 	{
-		$arr2 = array_flip(self::ATTRIBUTES);
+		$attributes = array('from', 'to');
+		$arr2 = array_flip($attributes);
 		$arr3 = array_diff_key($arr2, $attributes);
 		return array_diff_key($arr3, $attributes);
 	}
